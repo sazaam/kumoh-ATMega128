d685dd5aa93f5c89d36697d3a0d4c4f2b458e6c3
sazaam <sazaam@gmail.com>
2023-09-13 14:31:05 +0900

    Updated to full exercise Week02_LED_patterns.c

--- Week02_LED_patterns.c
+++ Week02_LED_patterns.c
@@ -44,38 +44,78 @@ int main(void){
 
 
 int main(void){
-	unsigned int ind = 256 ;
-	int hi = 256;
+	unsigned int ind = 128 ;
+	int hi = ind;
 	int lo = 1;
 	int up = 0;
+	
+	int i = 0;	
+
 	DDRA = 0xFF ;
 	
 	srand(100);
 
-	int pattern = 0 ;
+	int index = 0;
+	
+	
 	
 	do{
+		int pattern = (index/30) % 3 ;
+
 		switch(pattern){
 			case 0 :
 				PORTA = ind;
 		 
 				ind = up ? ind * 2  : ind / 2 ;
-		
-				if(ind <= 0 || ind >= 255){
+				
+				if(ind <= lo || ind >= hi){
 					up = !up ;
 					ind = up ? lo : hi;
 				}
 			break;
+			case 1 :
+				ind = 0;
+				
+				int ascend = 0, descend = 0;
+
+				for(int n = 0 ; n < i ; n++){
+					
+					ascend = ascend == 0 ? 1 : ascend;					
+					descend = descend == 0 ? 128 : descend;
+				
+					ind |= ascend ;
+					ascend = ascend << 1 ;
+					
+					ind |= descend ;
+					descend = descend >> 1;
+					
+				}
+				
+				PORTA = ind;
+				
+				up ? i-- : i++ ;
+
+				if(i >= 4 || i < 1) up = !up ;
+
+			break;
+			case 2 :
+				ind = rand( ) % 256 ;
+				
+				PORTA = ind;
+				
+				up ? i-- : i++ ;
+
+				if(i >= 4 || i < 1) up = !up ;
+
+			break;
 	
 		}
+
+		index ++;
 		
-		
-		_delay_ms(20) ; 
+		_delay_ms(50) ; 
 		
 	}while(1);
 	
 
 }
-
-
-
