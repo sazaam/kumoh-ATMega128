1509cd3e750a3c3d288b27f5f3ccd7bc60d4e12f
sazaam <sazaam@gmail.com>
2023-09-21 20:01:42 +0900

    Create Week_03_Interrupts.c

---
+++ Week_03_Interrupts.c
@@ -0,0 +1,43 @@
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+
+/////////////////////////////////// INTERRUPTS
+
+
+
+int main(){
+	
+	/*
+	// SWITCHES on PORT
+		
+	DDRA = 0xFF;
+	DDRE = 0x00;
+	
+	
+	// SETTINGS
+	
+
+	while(1){
+		
+		if((PINE & 0x10) == 0x00) // Port IN E (on Top)
+			PORTA = 0x80;
+		else if((PINE & 0x20) == 0x00) // Port IN E (on Bottom)
+			PORTA = 0x01;
+		else
+			PORTA = 0x00;
+
+	};
+
+   	*/
+	
+
+
+
+
+
+
+
+}
+
