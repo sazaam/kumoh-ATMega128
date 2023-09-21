dbe73c405ed8f952c062bdfb2d793eeb2ab36c60
sazaam <sazaam@gmail.com>
2023-09-21 11:43:10 +0900

    Update Week_03_TimerClock.c

--- Week_03_TimerClock.c
+++ Week_03_TimerClock.c
@@ -7,26 +7,16 @@ unsigned char fnd_sel[4] = {0x01, 0x02, 0x04, 0x08};
 unsigned char fnd[4];
 
 
+unsigned int toSeconds(int m, int s){ return m * 60 + s ; }
 
-int num2time(unsigned char arr[], int time){
+void num2time(unsigned char arr[], unsigned int time){
 	
+	arr[3] = (time / 600)	 %	 6;
+	arr[2] = (time / 60)	 %	10;
+	arr[1] = (time / 10)	 %	 6;
+	arr[0] = (time / 1)		 % 	10;
 
-	
-	
-	int fracTen = (time / 10) % 10;
-	int fracUn = (time) % 10;
-	int frac = fracTen * 10 + fracUn ;
-	
-	//arr[3] = (time / (120 * 60 * 10) % 6);
-	arr[3] = 0;
-	arr[2] = (time / (120 * 60) % 10);
-	arr[1] = (time / (1200)) % 6;
-	arr[0] = (time / 120) % 10;
-	
-	
-	return frac; // fraction of 1 second (time in milliseconds)
 }
-
 
 
 int main(){
@@ -34,40 +24,40 @@ int main(){
 	
 	DDRC = 0xFF;
 	DDRG = 0x0F;
-
-	int min = 4 ;
-	int sec = 31 ;
-	int start = (min * 120 * 60) + sec * 120;
+	
 	int i = 0 ;
-	
-	
+	int on = 1, remains = 0;
 
-	int cnt = start;
+	unsigned int start = toSeconds(59, 57);
+	unsigned int t = start;
+	unsigned int cnt = 0;
 	
+	while(1){
+		
+		cnt = (cnt >= 3600) ? 0 : cnt + 1;		// ensure count is not incrementing until reaches unsigned int overflow
 
-	do{
-		cnt++;
+		if(cnt % 360 == 0) { t = (t >= 3600) ? 1 : t + 1 ;}			// set a different timer
 		
-		//if(cnt == max) cnt = 0;
-		
-		
-		int on = 1 ;
-		
-		int rem = num2time(fnd, cnt);
-		
-		// on = rem < 50; // remainer of seconds
+		remains = cnt % 360 ; 					// remainer of fraction of second (here 1 second is 360 subseconds !!)
+		// on = remains < 180; 
 		on = 1;
 
-		for(i = 0 ; i < 4 ; i++){
-			PORTC = on ? digit[fnd[i]] : 0x00;
-			PORTG = on ? fnd_sel[i] : 0x00 ;
-			
-			_delay_ms(2);
-		}
+		num2time(fnd, t); 					// setting the array of time values
 		
 		
 
-	}while(1);	
+		int def =0x00 ;
+		int def2 = 0x00 ;
+
+		for(i = 0 ; i < 4 ; i++){
+			PORTC = on ? digit[fnd[i]] : def;
+			PORTG = on ? fnd_sel[i] : def2 ;
+			
+			_delay_ms(1);
+		}
+		
+
+	};
 	
 }
 
