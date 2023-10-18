af2082ad7c9fc71a0570924a63dde22c64d6742e
sazaam <sazaam@gmail.com>
2023-10-18 13:37:20 +0900

    Update Week_05_gugudan.c

--- Week_05_gugudan.c
+++ Week_05_gugudan.c
@@ -1,83 +1,74 @@
+
+
+
+/// INCLUDES
+
 #include <avr/io.h>
 #include <stdio.h>
 
 
-#define NULL 0
 
-static int putchar0( char c , FILE *stream ) ;
-
-static FILE mystdout = FDEV_SETUP_STREAM(putchar0, NULL, _FDEV_SETUP_WRITE);
-
-
-
-
-
-
+/// PRINTF Implementation
 char nl = '\n', cr = '\r' ;
-
+static int putchar0( char c , FILE *stream ) ;
+static FILE realstdout = FDEV_SETUP_STREAM(putchar0, NULL, _FDEV_SETUP_WRITE);
 
 int putchar0( char c , FILE *stream ){
-
-	
 	if(c == nl){ putchar0(cr, stream); }
-
 	while(!(UCSR0A & (1 << UDRE0)));
 	UDR0 = c ;
-
-
 	return 0;
 }
-
-
 char getchar0(){
 
 	while(!(UCSR0A & (1 << RXC0)));
 	return (UDR0) ;
 
 }
+/// END PRINTF Implementation
 
 
+
+
+//////// SETTINGS
 void init_uart(){
-
-	//////// Settings
-
 	UBRR0H = 0;
 	UBRR0L = 8;
 
 	UCSR0B = 0x18;
 	UCSR0C = 0x06;
-
-	////////
-
 }
+//////// END SETTINGS
+
+// check
+
+
 
 int main(){
 	
-	char prompt[] = "JKIT-128-1> ";
-	char *ptr;
-	char c;
-
+    char c; int i = 0, max = 10;
 	//////// Settings
-
 	init_uart();
-
+    //////// Printf Localizing
+    stdout = &realstdout;
 	////////
 
 
-	char c; int i;
 	
-	stdout = &mystdout;
+	
+	
 
 	while(1){
+        printf("Number for GUGUDAN : ");
 		c = getchar0() ;
-
-		putchar0(c);
+        printf("%c\n", c);
 		
-		if(c == '\r'){
-			putchar0('\n');
-
-			ptr = prompt;
-			puts0(ptr);
+        c = c - 0;
+		
+		if(c > 1 && c < max){
+			for(int i = 0 ; i < max; i++){
+                printf("\t%d x %d = %d\n", c, i, c*i);
+            }
 		}
 
 
