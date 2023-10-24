f79d9f9e5d902b03ca1635d87c9326a74bc877e6
sazaam <sazaam@gmail.com>
2023-10-24 13:57:01 +0900

    Update Week_7_Exam_Test_Input_Sentence_Uppercase.c

--- Week_7_Exam_Test_Input_Sentence_Uppercase.c
+++ Week_7_Exam_Test_Input_Sentence_Uppercase.c
@@ -9,11 +9,6 @@ unsigned char fnd_sel[4] = {0x01,0x02,0x04,0x08};
 static int putchar0(char c, FILE *stream);
 static FILE mystdout = FDEV_SETUP_STREAM(putchar0, NULL, _FDEV_SETUP_WRITE);
 
-
-
-volatile unsigned char c, oldc;
-volatile unsigned char cta = 0, ctA = 0, cto = 0;
-
 int putchar0(char c, FILE *stream)
 {
 
@@ -25,49 +20,126 @@ int putchar0(char c, FILE *stream)
 	return 0;
 }
 
+////////////// REQUIRED
+volatile unsigned char mem[25] = {0} ;
+volatile unsigned char c, oldc;
+volatile unsigned char cta = 0, ctA = 0, cto = 0;
 
-char getchar0()
-{
-	while (!(UCSR0A & (1<<RXC0)));
-	return(UDR0);
+
+
+//////////////////////// MEMORY HANDLING
+int mem_push(int n){
+	
+	for(int i = -1; i < 25 ; i++){
+		if(i == -1){
+			oldc = mem[0] ;
+		}else if(i == (25-1)){
+			mem[i] = n ;
+		}else mem[i] = mem[i+1] ;
+	}
+	return n ;
 }
 
-int check(unsigned char c){
+int mem_back(){
+	int n = 0;
+	for(int i = 25; i >= 0 ; i--){
+		if(i == 0) mem[0] = oldc ;
+		else if(i == 25) n = mem[i-1] ;
+		else mem[i] = mem[i-1] ;
+	}
+	return n ;
+}
 
+int mem_used_len(){ 
+	int l = 0 ;
+	for(int i = 0; i < 25 ; i++) if(mem[i] != 0 && mem[i] != ' ') l++ ;
+	return l;
+}
+
+void debug_mem(){ for(int i = 0; i < 25 ; i++) printf("%c, ", mem[i]) ; }
+void clear_mem(){ for(int i = 0; i < 25 ; i++) mem[i] = 0 ; }
+//////////////////////// END MEMORY HANDLING
+
+
+//////////////////////// HANDLING EXITING
+int checkEnd(){
+	int len = 3 ;
+	unsigned char word[3] = {'e', 'n', 'd'} ;
+	int flag = mem_used_len() <= len ;
+	while(len--){
+		char mem_c = mem[25-(3-len)] ;
+		int f = mem_c == word[len] ;
+		flag &= f ;
+	}
+	return flag ;
+}
+
+
+void exiting(){
+	printf("\n\t exiting...\n") ;
+	UCSR0B = 0x0; // this disables the whole session it seems
+}
+/////////////////////// END HANDLING EXITING
+
+
+/////////////////////// PROGRAM LOGIC
+int checkCharType(unsigned char c){
     return 
-        (c >= 'a' && c <= 'z') ? 1 :
-        (c >= 'A' && c <= 'Z') ? 2 : 3 ;
-
+        (c >= 'A' && c <= 'Z') ? 1 :
+        (c >= 'a' && c <= 'z') ? 2 : 0 ;
 }
+void erase(){
+	// basckspace case
+    int n = checkCharType(mem_back()) ;
+    if(n == 1) ctA-- ;
+    else if(n == 2) cta-- ;
+    else cto--;
+}
+void check(unsigned char c){
+	int n = checkCharType(c) ;
+	if(n == 1) ctA++ ;
+	else if(n == 2) cta++ ;
+	else cto++;
+	mem_push(c) ;
+}
+void reset(){
+	cta = ctA = cto = 0 ;
+	clear_mem() ;
+    printf("\nPlease Input a Sentence :") ;
+}
+void evaluate(){
+	
+	// debug_mem() ; // if needed
+
+	if(checkEnd()) return exiting();
+	
+	printf("\n[lowercase] : %d\n", cta) ;
+	printf("\n[UPPERCASE] : %d\n", ctA) ;
+	printf("\n[!@#$~] : %d\n", cto) ;
+}
+/////////////////////// END PROGRAM LOGIC
+
+
 
 ISR(USART0_RX_vect){
     cli();
     c = UDR0;
-    int n = 0;
-    if(c=='\r'){
-        printf("\n[a-z] : %d\n", cta) ;
-        printf("\n[A-Z] : %d\n", ctA) ;
-        printf("\n[^a-Z] : %d\n", cto) ;
-        cta = ctA = cto = 0;
-        printf("\nPlease Input a Sentence :") ;
-        
-    } else {
-        printf("%c", c) ;
-        if(c == 127){
-            // basckspace case
-            n = check(oldc) ;
-            if(n == 1) cta-- ;
-            if(n == 2) ctA-- ;
-            else cto--;
-        }else{
-            n = check(c) ;
-            if(n == 1) cta++ ;
-            if(n == 2) ctA++ ;
-            else cto++;
-        }
-        oldc = c ;
-        
+	
+	switch(c){
+		case '\r': 
+			evaluate() ; 
+			reset() ; 
+			break ;
+		case 127: 
+			printf("%c", c) ; 
+			erase() ; 
+			break ;
+		default:
+			printf("%c", c) ; 
+			check(c) ; 
+			break ;
     }
+
     sei();
 }
 
@@ -107,19 +179,18 @@ int main()
     settings(); 
 
 	/* Putty Settings : 
-		COM3
+		COM{port} // check on computer
 		Data Bit 8
 		Unit Bit 1
 		Parity None
 		Last None
 	*/
 
-	printf("\nPlease Input a Sentence :") ;    
+	reset() ;
 
     while(1)
 	{
 		//
 	}
 
-    
 }
