307b282cef70581b1e966a1478ca09fca27b7862
sazaam <sazaam@gmail.com>
2024-03-13 10:50:12 +0900

    Update Week_02_Bright_Sensor_States.c

--- SEMESTER 2/Week_02_Bright_Sensor_States.c
+++ SEMESTER 2/Week_02_Bright_Sensor_States.c
@@ -22,6 +22,23 @@ int putchar0(char c, FILE *stream){
 
 
 
+/*
+
+Extra : FND Display of the sensor Value
+
+Today I wanted to see if I could also display the value in the FND leds,
+So ended up putting the display function in the ADC Interrupt, 
+and transform _delay_ms(500) into 
+a more fancy loop calculation (10ms * 50)
+Since I need more frequency for the display loop.
+
+*/
+
+
+
+
+
+
 //////// SETTINGS
 
 
@@ -79,16 +96,18 @@ void initFND(){
 
 void fnd_arr(unsigned int arr[], int n){
 	double l = 3 ;
-	while(l--){
-		arr[(int) l] = (int) (n / pow(10, l)) % 10 ; // replace multiple lines with calculations through powers of 10 => 10^3 = 1000 , 10^0 = 1
+	//while(l--){
+		//arr[(int) l] = (int) (n / pow(10, l)) % 10 ; // replace multiple lines with calculations through powers of 10 => 10^3 = 1000 , 10^0 = 1
 		// But beware !! The arguments and the output of pow() are of type DOUBLE !!!
-	}
+	//}
+
+	// 0~1023
 	
-	// arr[0] = (n / 1000) % 10 ;
-	// arr[1] = (n / 100) % 10 ;
-	// arr[2] = (n / 10) % 10 ;
-	// arr[3] = (n / 1) % 10 ;
-	
+	arr[3] = (n / 1000) % 10 ;
+	arr[2] = (n / 100) % 10 ;
+	arr[1] = (n / 10) % 10 ;
+	arr[0] = (n / 1) % 10 ;
+
 	return 0 ;
 }
 
@@ -127,6 +146,8 @@ volatile unsigned short prev ;
 
 
 
+#define FND_LEN 4
+
 
 
 //////// INTERRUPT 4 ADC
@@ -134,40 +155,73 @@ ISR(ADC_vect){
 
 	cli() ;
 	
-	adc_lo = ADCL ;
-	adc_hi = ADCH ;
 	
-	unsigned short n = (adc_hi << 8 ) | adc_lo ;
-		/*
+	
+	
+	
+	
+		
+	/// FND	
+	int max = FND_LEN - 1 ;
+	
+	
+	// Final Loop in displaying FND will take ~10ms so multiply by 50 times in order to have an approx 500ms timespan between displays
+	for(int i = 0 ; i < 50 ; i++){
+		if (i == 0) {
+			
+
+			//////////////////////////// Sensor Incoming Value Handling
+			adc_lo = ADCL ;
+			adc_hi = ADCH ;
+	
+			unsigned short n = (adc_hi << 8 ) | adc_lo ;
+				/*
 		
 		
-		Implemented a Safe Margin State Handler
-		Within a 50 units range, Brighter or Darker will not trigger 
-		but will remain under Stable state.
+				Implemented a Safe Margin State Handler
+				Within a 50 units range, Brighter or Darker will not trigger 
+				but will remain under Stable state.
 		
-		*/
-	int margin = 50 ;
-	int state = 0 ;
-	if(n >= prev + margin){
-		state = 2 ;
-		prev = n ;
-	}else if(n <= prev - margin){
-		state = 1 ;
-		prev = n ;
+				*/
+			int margin = 50 ;
+			int state = 0 ;
+			if(n >= prev + margin){
+				state = 2 ;
+				prev = n ;
+			}else if(n <= prev - margin){
+				state = 1 ;
+				prev = n ;
+			}
+	
+			///// LED Debug display
+			showADC(n) ;
+
+			///// Send into Serial Print Interface
+			printADC(n, state, n - (int)prev) ;
+			
+			
+
+			////// Write value into fnd Array
+			fnd_arr(fnd, n) ;
+		}
+		
+
+		/////// FND DISPLAY
+
+		int l = max ;
+		for(; l >= 0 ; --l){
+		
+			PORTC = digit[fnd[l]] ;
+			PORTG = 0x01 << l;
+
+			if(l % 2){
+				_delay_ms(2) ;
+			}else{
+				_delay_ms(3) ;
+			}
+		}
+		_delay_ms(1) ;
 	}
-	
-	///// LED Debug display
-	showADC(n) ;
-
-	///// Send into Serial Print Interface
-	printADC(n, state, n - (int)prev) ;
-	
-
-	///// Write FND Display Array
-	fnd_arr(fnd, n) ;
-
-	// wait for a half second
-	_delay_ms(500) ;
 	
 
 	sei() ;
@@ -176,8 +230,6 @@ ISR(ADC_vect){
 
 
 
-
-#define FND_LEN 4
 
 int main(){
 	
@@ -191,30 +243,11 @@ int main(){
 	
 
 	
-	int max = FND_LEN - 1 ;
-	int l = max ;
-	
 
 	while(1){
+		// Reset SENSOR
 		ADCSRA |= (1 << ADSC) ;
 
-
-		/// FND
-		
-		for(; l >= 0 ; --l){
-			
-			PORTC = digit[fnd[l]] ;
-			PORTG = 0x01 << l;
-
-			if(l % 2){
-				_delay_ms(2) ;
-			}else{
-				_delay_ms(3) ;
-			}
-			
-		}
-		
-		_delay_ms(1) ;
 	}
 
 	return 0;
